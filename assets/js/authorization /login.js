ac7ed0bdc20f9b0e26ec2a7123fc92e9ea0d1e35
ecl5pse <fantamas20@gmail.com>
2019-12-16 20:20:32 +0200

    modif project

---
+++ assets/js/authorization /login.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const  formElement = document.forms[0];
+
+
+
+formElement.onsubmit = {} =>{
+	const  userFields = ['email' , 'password'];
+
+	const  user = {};
+
+	localStorage.setItem()
+
+
+}
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,25 @@
 'use strict';
+
+
+
+let timeoutID = null;
+document.addEventListener('mousemove',(e)=>{
+
+	if(timeoutID){
+		clearTimeout(timeoutID);
+
+	}
+
+	timeoutID = setTimeout({}  =>{
+		location.assign(`${location.origin}/pages/authorization/login.html`);
+	}, 4000);
+
+
+});
+
+
+
+
+
+
+
